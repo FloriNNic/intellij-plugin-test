e48b98ddc36f2d7494841e89ff41d3204d17020e
GitHub Action <action@github.com>
2023-01-20 15:58:16 +0000

    Template cleanup

--- src/main/kotlin/com/github/florinnic/intellijplugintest/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/florinnic/intellijplugintest/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.florinnic.intellijplugintest.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.florinnic.intellijplugintest.MyBundle
 
 class MyApplicationService {
 
